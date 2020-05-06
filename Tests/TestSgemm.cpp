abab790fab8b9dea69cc51b580ee5f5f34990a40
Yu Li <lyo.gavin@gmail.com>
2020-05-06 11:06:03 -0700

    add test

--- Tests/TestSgemm.cpp
+++ Tests/TestSgemm.cpp
@@ -1,86 +1,39 @@
 #include <QPULib.h>
-#include <sys/time.h>
-#include <math.h>
+#include <cstring.h>
 
-// #define USE_SCALAR_VERSION
-
-// ============================================================================
-// Scalar version
-// ============================================================================
-
-void rot3D(int n, float cosTheta, float sinTheta, float* x, float* y)
-{
-  for (int i = 0; i < n; i++) {
-    float xOld = x[i];
-    float yOld = y[i];
-    x[i] = xOld * cosTheta - yOld * sinTheta;
-    y[i] = yOld * cosTheta + xOld * sinTheta;
-  }
-}
-
-// ============================================================================
-// Vector version 1
-// ============================================================================
-
-void rot3D_1(Int n, Float cosTheta, Float sinTheta, Ptr<Float> x, Ptr<Float> y)
-{
-  For (Int i = 0, i < n, i = i+16)
-    Float xOld = x[i];
-    Float yOld = y[i];
-    x[i] = xOld * cosTheta - yOld * sinTheta;
-    y[i] = yOld * cosTheta + xOld * sinTheta;
-  End
-}
-
-// ============================================================================
-// Vector version 2
-// ============================================================================
-
-void rot3D_2(Int n, Float cosTheta, Float sinTheta, Ptr<Float> x, Ptr<Float> y)
-{
-  Int inc = 16;
-  Ptr<Float> p = x + index();
-  Ptr<Float> q = y + index();
-  gather(p); gather(q);
- 
-  Float xOld, yOld;
-  For (Int i = 0, i < n, i = i+inc)
-    gather(p+inc); gather(q+inc); 
-    receive(xOld); receive(yOld);
-    store(xOld * cosTheta - yOld * sinTheta, p);
-    store(yOld * cosTheta + xOld * sinTheta, q);
-    p = p+inc; q = q+inc;
-  End
-
-  receive(xOld); receive(yOld);
-}
-
-// ============================================================================
-// Vector version 3
-// ============================================================================
-
-void rot3D_3(Int n, Float cosTheta, Float sinTheta, Ptr<Float> x, Ptr<Float> y)
-{
-  Int inc = numQPUs() << 4;
-  Ptr<Float> p = x + index() + (me() << 4);
-  Ptr<Float> q = y + index() + (me() << 4);
-  gather(p); gather(q);
- 
-  Float xOld, yOld;
-  For (Int i = 0, i < n, i = i+inc)
-    gather(p+inc); gather(q+inc); 
-    receive(xOld); receive(yOld);
-    store(xOld * cosTheta - yOld * sinTheta, p);
-    store(yOld * cosTheta + xOld * sinTheta, q);
-    p = p+inc; q = q+inc;
-  End
-
-  receive(xOld); receive(yOld);
-}
 
 // ============================================================================
 // Main
 // ============================================================================
+
+void conv1x1s1_sgemm_cpu(float* bottom_blob, float* top_blob, float* kernel, float* bias, int w, int h, int inch, int outch)
+{
+    for (int k = 0; k < outch; k++) {
+        for (int j = 0; j < inch; j++) {
+            for (int i = 0; i < w; i++) {
+                for (int l = 0; l < h; l++) {
+                    top_blob[w*h*k + w*l + i] = bias[k];
+                }
+            }
+            for (int i = 0; i < w; i++) {
+                for (int l = 0; l < h; l++) {
+                    top_blob[w*h*k + w*l + i] += bottom_blob[w*h*j + w*l + i] * kernel[k * inch + j]
+                }
+            }
+        }
+    }
+}
+
+//void conv1x1s1_sgemm_qpu(void* bottom_blob, void* top_blob, void* kernel, void* bias, int w, int h, int inch, int outch, int elemsize)
+
+float get_diff(float* input, float* output, int size) {
+    float diff = 0;
+    for(int i = 0;i < size;i++) {
+        diff += output[i] - intput[i];
+    }
+
+    return diff;
+}
 
 int main()
 {
@@ -91,43 +44,41 @@ int main()
   const int N = 19200; // 192000
   const float THETA = (float) 3.14159;
 
-#ifdef USE_SCALAR_VERSION
-  // Allocate and initialise
-  float* x = new float [N];
-  float* y = new float [N];
-  for (int i = 0; i < N; i++) {
-    x[i] = (float) i;
-    y[i] = (float) i;
-  }
-#else
-  // Construct kernel
-  auto k = compile(rot3D_3);
+  const int w = 10, h=10, inch=10, outch=10,elemsize=4;
 
-  // Use 12 QPUs
-  k.setNumQPUs(12);
+  float* bot = new(w*h*inch);
+  float* top = new(w*h*outch);
+  float* ker = new(outch*inch);
+  float* bias = new(outch);
 
-  // Allocate and initialise arrays shared between ARM and GPU
-  SharedArray<float> x(N), y(N);
-  for (int i = 0; i < N; i++) {
-    x[i] = (float) i;
-    y[i] = (float) i;
-  }
-#endif
+
+  float* botcpu = new(w*h*inch);
+  memcpy(botcpu, bot);
+  float* topcpu = new(w*h*outch);
+  memcpy(topcpu, top);
+  float* kercpu = new(outch*inch);
+  memcpy(kercpu, ker);
+  float* biascpu = new(outch);
+  memcpy(biascpu, bias);
+
+  conv1x1s1_sgemm_cpu(bot, top, ker, bias, w, h, inch, outch, sizeof(float));
+
+
 
   gettimeofday(&tvStart, NULL);
-#ifdef USE_SCALAR_VERSION
-  rot3D(N, cosf(THETA), sinf(THETA), x, y);
-#else
-  k(N, cosf(THETA), sinf(THETA), &x, &y);
-#endif
+
+  conv1x1s1_sgemm_cpu(botcpu, topcpu, kercpu, biascpu, w, h, inch, outch);
+
   gettimeofday(&tvEnd, NULL);
   timersub(&tvEnd, &tvStart, &tvDiff);
 
   // Display results
   //for (int i = 0; i < N; i++)
   //  printf("%f %f\n", x[i], y[i]);
+
+  float diff = get_diff(top, topcpu, w*h*outch);
  
-  printf("%ld.%06lds\n", tvDiff.tv_sec, tvDiff.tv_usec);
+  printf("diff: %f, %ld.%06lds\n", diff, tvDiff.tv_sec, tvDiff.tv_usec);
 
   return 0;
 }

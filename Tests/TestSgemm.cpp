45e7f0971a9a18b5c54fc68cd0a73892c648715b
Yu Li <lyo.gavin@gmail.com>
2020-05-06 18:04:03 -0700

    fix memcpy

--- Tests/TestSgemm.cpp
+++ Tests/TestSgemm.cpp
@@ -34,7 +34,11 @@ void conv1x1s1_sgemm_cpu(float* bottom_blob, float* top_blob, float* kernel, flo
 float get_diff(float* input, float* output, int size) {
     float diff = 0;
     for(int i = 0;i < size;i++) {
-        diff += output[i] - input[i];
+        single_diff = output[i] - input[i];
+        if (single_diff > 0.0001f){
+            printf("diff pos: %d", i);
+        }
+        diff += single_diff;
     }
 
     return diff;

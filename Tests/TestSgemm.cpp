90be877a9e11e534377f8158b6cdfa98f2ea27b6
Yu Li <lyo.gavin@gmail.com>
2020-05-06 17:39:51 -0700

    fix memcpy

--- Tests/TestSgemm.cpp
+++ Tests/TestSgemm.cpp
@@ -57,7 +57,7 @@ int main()
   // Number of vertices and angle of rotation
   const float THETA = (float) 3.14159;
 
-  const int w = 1, h=1, inch=1, outch=1,elemsize=4;
+  const int w = 10, h=10, inch=10, outch=10,elemsize=4;
 
   float* bot = new float[w*h*inch];
   fill_rand(bot, w*h*inch);

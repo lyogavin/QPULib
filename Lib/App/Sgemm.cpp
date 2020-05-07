a97ec53e3e524e2445d201dc1f326f38fd4de986
Yu Li <lyo.gavin@gmail.com>
2020-05-06 21:47:51 -0700

    fix memcpy

--- Lib/App/Sgemm.cpp
+++ Lib/App/Sgemm.cpp
@@ -84,7 +84,8 @@ void conv1x1s1_sgemm_qpulib(Ptr<Float> bottom, Ptr<Float> top, Ptr<Float> kernel
                     Print(m);
                     Print("\n");
                     Print("top_last_by_one:");
-                    Print(*top_last_by_one);
+                    Float v = *top_last_by_one;
+                    Print(v);
                     Print("\n");
                     store(bottom_last_by_one * kernel_last + top_last_by_one, top_ptr_by_one);
                 End

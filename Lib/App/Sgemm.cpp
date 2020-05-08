0458871958c5ae4e3a79ef95e879718cc8fe500f
Yu Li <lyo.gavin@gmail.com>
2020-05-08 08:24:49 -0700

    fix memcpy

--- Lib/App/Sgemm.cpp
+++ Lib/App/Sgemm.cpp
@@ -245,7 +245,7 @@ void conv1x1s1_sgemm_qpu(float* bottom_blob, float* top_blob, float* kernel, flo
 {
     int padding = 16;
     int total = w * h;
-    int NQPUS = 12;
+    int NQPUS = 1;
 
     // Timestamps
     timeval tvStart, tvEnd, tvDiff;
